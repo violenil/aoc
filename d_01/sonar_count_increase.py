5cadf5969cba072980af49852f5ef414d0d40714
violenil <retrospect@protonmail.com>
2021-12-08 14:10:52 +0100

    update Day 1 script to be more sightly

--- d_01/sonar_count_increase.py
+++ d_01/sonar_count_increase.py
@@ -1,11 +1,4 @@
-with open("sonar_input", 'r') as inF:
-    lines = inF.readlines()
-    sonar_readings = [int(s) for s in lines]
-
-assert type(sonar_readings[10]) == int
-
 def count_inc(readings):
-    
     inc_count = 0
     prev = None
 
@@ -16,10 +9,10 @@ def count_inc(readings):
         if r > prev:
             inc_count += 1
         prev = r
+
     return(inc_count)
 
 def count_inc_window(window, readings):
-    
     assert type(window) == int
     sum_readings = []
     for r in range(len(readings)):
@@ -27,14 +20,23 @@ def count_inc_window(window, readings):
             continue
         sum_window = sum(readings[r:r+window])
         sum_readings.append(sum_window)
-    
-    print(sum_readings)
-    print("number of sums: ", len(sum_readings))
-
     inc_count = count_inc(sum_readings)
 
     return(inc_count)
 
+if __name__ == "__main__":
+    # read input file
+    with open("sonar_input", 'r') as inF:
+        lines = inF.readlines()
+        sonar_readings = [int(s) for s in lines]
 
-inc_count = count_inc_window(3, sonar_readings)
-print(inc_count)
+    # Solution to Part A
+    print("Solution to Day 1 Part A")
+    inc_count = count_inc(sonar_readings)
+    print(f"Depth increase number: {inc_count}\n")
+
+    # Solution to Part B
+    print("Solution to Day 2 Part B")
+    window = 3
+    inc_window_count = count_inc_window(window, sonar_readings)
+    print(f"Depth increase over window ({window}): {inc_window_count}\n")
