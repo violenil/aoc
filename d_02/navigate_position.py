a4a59ee6bc82561ab30f05043dd57d8ecfedfcba
violenil <retrospect@protonmail.com>
2021-12-08 13:59:14 +0100

    update and prettify the solution

--- d_02/navigate_position.py
+++ d_02/navigate_position.py
@@ -1,4 +1,4 @@
-def calculate_position(pos_movements):
+def calculate_position(pos_movements, aim:int=0, use_aim:bool=False):
     horizontal_pos = 0
     depth = 0
 
@@ -6,19 +6,35 @@ def calculate_position(pos_movements):
         direction, count = m.strip().split(" ")
         if direction == "forward":
             horizontal_pos += int(count)
-        elif direction == "down":
-            depth += int(count)
-        elif direction == "up":
-            depth -= int(count)
+            if use_aim:
+                depth += int(count)*aim
 
+        elif direction == "down":
+            if use_aim:
+                aim += int(count)
+            else:
+                depth += int(count)
+
+        elif direction == "up":
+            if use_aim:
+                aim -= int(count)
+            else:
+                depth -= int(count)
     print(f"Horizontal Position: {horizontal_pos}")
     print(f"Depth: {depth}")
-    
     return horizontal_pos*depth
-
 
 with open("navigate_input", 'r') as inF:
     position_instructions = inF.readlines()
 
-final_depth_hor = calculate_position(position_instructions)
-print(final_depth_hor)
+
+if __name__ == "__main__":
+    # To solve Part A:
+    print("Solution to Day 02 Part A")
+    final_depth_hor = calculate_position(position_instructions, use_aim=False)
+    print(f"Final solution: {final_depth_hor}\n")
+
+    # To solve Part B:
+    print("Solution to Day 02 Part B")
+    final_depth_hor = calculate_position(position_instructions, use_aim=True)
+    print(f"Final solution: {final_depth_hor}\n")
